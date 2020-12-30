27132878f0dbe6ade3db4af0fd56473ab1ccada7
Jaime Munoz <jaimungon@gmail.com>
2020-12-30 15:16:38 +0100

    add cors library

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import helmet from 'helmet'
-import compression from 'compression'
+import compress from 'compression'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -13,10 +14,11 @@ const app = express()
 const db = mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const port = process.env.PORT || 3000
 
-app.use(helmet())
-app.use(compression())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(helmet())
+app.use(compress())
+app.use(cors())
 
 app.use('/', coursesRoute)
 app.use('/', authorsRoute)
