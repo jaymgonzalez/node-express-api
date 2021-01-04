e30192f92c23d20aa95a4b02ef57f35581049266
Jaime Munoz <jaimungon@gmail.com>
2021-01-04 12:29:27 +0100

    add auth routes and user model

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv'
 import coursesRoute from './routes/courses.routes.js'
 import authorsRoute from './routes/authors.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import compress from 'compression'
 import cors from 'cors'
@@ -16,13 +19,15 @@ const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(helmet())
 app.use(compress())
 app.use(cors())
 
 app.use('/', coursesRoute)
 app.use('/', authorsRoute)
-
+app.use('/', authRoutes)
+app.use('/', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('Welcome to my Courses API')
@@ -31,3 +36,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
 })
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ "error": err.name + ": " + err.message })
+  } else if (err) {
+    res.status(400).json({ "error": err.name + ": " + err.message })
+    console.log(err)
+  }
+})
+
+export default app
