346d9b4c07d34e8c92594d226a7b9fb9f7bbd924
Jaime Munoz <jaimungon@gmail.com>
2021-01-04 12:38:14 +0100

    add requiredSingin to course and author routes

--- routes/authors.routes.js
+++ routes/authors.routes.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import authorCtrl from '../controllers/author.controller.js'
+import authCtrl from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 router.route('/api/authors')
   .get(authorCtrl.list)
-  .post(authorCtrl.create)
+  .post(authCtrl.requireSignin, authorCtrl.create)
 
 router.route('/api/authors/:authorId')
   .get(authorCtrl.authorById)
-  .put(authorCtrl.update)
-  .delete(authorCtrl.remove)
+  .put(authCtrl.requireSignin, authorCtrl.update)
+  .delete(authCtrl.requireSignin, authorCtrl.remove)
 
 router.param('authorId', authorCtrl.setAuthorId)
 
--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import courseCtrl from '../controllers/course.controller.js'
+import authCtrl from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 router.route('/api/courses')
   .get(courseCtrl.list)
-  .post(courseCtrl.create)
+  .post(authCtrl.requireSignin, courseCtrl.create)
 
 router.route('/api/courses/:courseId')
   .get(courseCtrl.courseById)
-  .put(courseCtrl.update)
-  .delete(courseCtrl.remove)
+  .put(authCtrl.requireSignin, courseCtrl.update)
+  .delete(authCtrl.requireSignin, courseCtrl.remove)
 
 router.param('courseId', courseCtrl.setCourseId)
 
